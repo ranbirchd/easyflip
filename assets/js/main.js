3b40b449f2a2d8765d7c8a76ae6b0c25b8bf9d59
ranbirchd <ranbirchd@gmail.com>
2021-04-01 11:10:04 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,32 @@
+/*
+	Photon by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-$(document).ready(function() {
-	
-    
-    /* ===== Smooth scrolling ====== */
-	$('a.scrollto').on('click', function(e){
-        //store hash
-        var target = this.hash;    
-        e.preventDefault();
-		$('body').scrollTo(target, 800, {offset: 0, 'axis':'y'});
+(function($) {
 
-	});
-	
-    
-    
+	var	$window = $(window),
+		$body = $('body');
 
-});
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1141px',  '1680px' ],
+			large:    [ '981px',   '1140px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '321px',   '480px'  ],
+			xxsmall:  [ null,      '320px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+})(jQuery);
